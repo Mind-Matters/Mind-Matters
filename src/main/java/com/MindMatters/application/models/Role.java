217252f4708e353ac48e5b1d39aa4fa250e637e7
Kiley Hector <kiley.hector@gmail.com>
2023-02-06 12:25:05 -0500

    working on setting up db

---
+++ src/main/java/com/MindMatters/application/models/Role.java
@@ -0,0 +1,42 @@
+package com.MindMatters.application.models;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name ="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String role_category;
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    public Role(long id, String role_category) {
+        this.id = id;
+        this.role_category = role_category;
+    }
+
+    public Role (){
+
+    }
+
+    public String getRole_category() {
+        return role_category;
+    }
+
+    public void setRole_category(String role_category) {
+        this.role_category = role_category;
+    }
+}
---
+++ src/migration.sql
@@ -0,0 +1,5 @@
+create database if not exists mind_db;
+
+    create user mind_user@localhost identified by 'password';
+
+        grant all on mind_db.* to mind_user@localhost;

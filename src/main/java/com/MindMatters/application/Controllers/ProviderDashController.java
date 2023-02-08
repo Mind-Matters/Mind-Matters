9c34aa14952225c24879f64d227bc571c5f63316
Kiley Hector <kiley.hector@gmail.com>
2023-02-08 17:44:23 -0500

    provider dashboard

---
+++ src/main/java/com/MindMatters/application/Controllers/ProviderDashController.java
@@ -0,0 +1,13 @@
+package com.MindMatters.application.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProviderDashController {
+    @GetMapping("/provider-dashboard")
+    public String goProviderDashboard(){
+        return "/provider-dashboard";
+    }
+
+}

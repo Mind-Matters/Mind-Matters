bb54ec76f0b52dbeef8f11dd63f43c0c0530e955
Andres Delgado <andresdelgado335@gmail.com>
2023-02-08 14:26:00 -0600

    finished linking signup form to database

--- src/main/java/com/MindMatters/application/Controllers/SignupController.java
+++ src/main/java/com/MindMatters/application/Controllers/SignupController.java
@@ -1,9 +1,24 @@
 package com.MindMatters.application.Controllers;
 
 
+import com.MindMatters.application.Models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController {
 
+//    @GetMapping("/signup")
+//    public String showSignupForm(Model model){
+//        model.addAttribute("user", new User());
+//        return "/signup";
+//    }
+//
+//    @PostMapping("/signup")
+//    public String createUser(@ModelAttribute User user){
+//        userDao.save(user);
+//    }
 }

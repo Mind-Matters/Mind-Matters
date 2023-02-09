a9fe18a6b767e45d5f896ced975e5f9e73d947ed
Brittney Brinson <brittneyc95@outlook.com>
2023-02-09 10:18:42 -0500

    Made the controller for the patient dashboard

---
+++ src/main/java/com/MindMatters/application/Controllers/PatientDashController.java
@@ -0,0 +1,12 @@
+package com.MindMatters.application.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PatientDashController {
+    @GetMapping("/patient-dashboard")
+    public String goPatientDashboard(){
+        return "/patient-dashboard";
+    }
+}

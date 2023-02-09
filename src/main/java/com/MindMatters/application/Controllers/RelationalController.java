29bf0fcd6174c1cf3ba6371ffda701694cb840d0
Andres Delgado <andresdelgado335@gmail.com>
2023-02-09 16:26:44 -0600

    fixing issues

--- src/main/java/com/MindMatters/application/Controllers/RelationalController.java
+++ src/main/java/com/MindMatters/application/Controllers/RelationalController.java
@@ -1,8 +1,13 @@
 package com.MindMatters.application.Controllers;
 
 
+
 import com.MindMatters.application.Controllers.Repositories.RelationalRepo;
+import com.MindMatters.application.Models.ProviderPatient;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class RelationalController {
@@ -15,6 +20,13 @@ public class RelationalController {
 
     public RelationalController(RelationalRepo relationalDao) {
         this.relationalDao = relationalDao;
+
+    }
+
+    @GetMapping("/signup")
+    public String assignPatient(Model model){
+        model.addAttribute("myProvider", new ProviderPatient());
+        return ("/signup");
 
     }
 }

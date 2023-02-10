962f4a83dbfd1385a38f21182dc845f5b8ee82e2
Brittney Brinson <brittneyc95@outlook.com>
2023-02-10 17:03:01 -0500

    Got the event to save to the database with help

--- src/main/java/com/MindMatters/application/Controllers/EventController.java
+++ src/main/java/com/MindMatters/application/Controllers/EventController.java
@@ -4,20 +4,38 @@ import com.MindMatters.application.Models.Event;
 import com.MindMatters.application.Controllers.Repositories.EventRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 @Controller
 public class EventController {
     private final EventRepo eventDao;
 
+    private Event event;
+
     public EventController(EventRepo eventDao) {
         this.eventDao = eventDao;
     }
 
+    @GetMapping ("/event-dashboard")
+    public String addEvent(Model model) {
+        model.addAttribute("event", new Event());
+        return "/patient-dashboard";
+    }
+    @PostMapping("/event-dashboard")
+    public String createEvent(@RequestParam(name = "title") String title,
+                              @RequestParam(name = "description") String description,
+                              @RequestParam(name = "date") String date
+    ){
+        Event event = new Event();
+        event.setTitle(title);
+        event.setDescription(description);
+        event.setDate(new Date());
+        eventDao.save(event);
+        return "redirect:/event-dashboard";
+    }
 /*  using Form Model Binding
     Abandoned this approach because we encountered errors. Need instructor assistsance to implement.
     @GetMapping("/add-event")

4ce9f1bf4919803c78a81733c64cf5a4fced2f0b
Miles Raker <milesraker@gmail.com>
2023-02-10 10:22:33 -0800

    created trackMedication model

---
+++ src/main/java/com/MindMatters/application/Models/trackMedication.java
@@ -0,0 +1,64 @@
+package com.MindMatters.application.Models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="track_medication")
+public class trackMedication {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(nullable = false)
+    private Boolean taken;
+
+    public trackMedication() {
+    }
+
+    public trackMedication(User user, Date date, Boolean taken) {
+        this.user = user;
+        this.date = date;
+        this.taken = taken;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean getTaken() {
+        return taken;
+    }
+
+    public void setTaken(Boolean taken) {
+        this.taken = taken;
+    }
+}

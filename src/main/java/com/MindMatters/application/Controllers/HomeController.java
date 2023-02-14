ae4c2c9b1978de19ac52809d4f1bd0a5fa8fadf7
Miles Raker <milesraker@gmail.com>
2023-02-14 10:15:33 -0800

    home controller is changed - again :)

--- src/main/java/com/MindMatters/application/Controllers/HomeController.java
+++ src/main/java/com/MindMatters/application/Controllers/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/home")
     public String goHome(){
-        return "index";
+        return "home";
     }
 }

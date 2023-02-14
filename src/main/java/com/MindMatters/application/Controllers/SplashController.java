873bf74329e749a226515044546e2f12fcb1152b
Miles Raker <milesraker@gmail.com>
2023-02-14 12:27:17 -0800

    push this to github? lets try

--- src/main/java/com/MindMatters/application/Controllers/SplashController.java
+++ src/main/java/com/MindMatters/application/Controllers/SplashController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SplashController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String renderSplash(){
 
         return "splash";

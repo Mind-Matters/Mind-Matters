6e73d8b32216e0627f696e110fa491e0f0ecab6f
Miles Raker <milesraker@gmail.com>
2023-02-16 10:03:08 -0800

    midway through error handling. Switching to other branch

--- src/main/java/com/MindMatters/application/controllers/AuthenticationController.java
+++ src/main/java/com/MindMatters/application/controllers/AuthenticationController.java
@@ -1,15 +1,22 @@
 package com.MindMatters.application.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model model) {
+        model.addAttribute("loginError", "");
         return "/login";
     }
 
+    @GetMapping("/login?error")
+    public String showLoginError(Model model){
+        model.addAttribute("loginError", "Invalid username or password.");
+        return "/login";
+    }
 
     @GetMapping("/logout")
     public String showLogout(){
